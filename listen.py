1cbe98de09e64bf4c64f14c04307530e3a179f68
Hesham Saad <90488401+heshamsaad98@users.noreply.github.com>
2022-08-04 21:23:21 -0700

    Create listen.py

---
+++ listen.py
@@ -0,0 +1,10 @@
+words = input("Saying something: ").lower()
+
+if "hello" in words:
+    print("Hello to yo too!")
+elif "how are you" in words:
+    print("I am well, thanks")
+elif "goodbye" in words:
+    print("Goodbye to you  too!")
+else:
+    print("Huh?")
